e0e2fa4510cf1b858cc9c909e09efe46f6ff600c
Jeenitkumar Khatri <jkhatri@infoblox.com>
2025-06-13 07:44:26 +0530

    Updated swagger main

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/nios-swagger/json/dns.json",
+    url: "nios-swagger/json/dns.json",
     urls: [
       {url: "nios-swagger/json/acl.json", name: "ACL"},
       {url: "nios-swagger/json/cloud.json", name: "Cloud"},
